70747d27d5de8f12eaa1bbbca19fa61adfcb4b89
Nikhil Vishwakarma <126031142+Nikhil22690@users.noreply.github.com>
2026-01-31 08:56:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,8 @@ terraform {
 provider "aws" {
   # alias      = "mumbai"
   region     = "ap-south-1"
-  access_key = "AKIA2P7GLS7YAJANMDFA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
+
 
